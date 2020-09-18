faee5968692487e31f111ac0c4974643d06ce487
Александр Филиппов <xperiaz1981>
2020-09-18 20:08:12 +0300

    commit uml

---
+++ src/ru/mirea/ikbo1319/pr4/z2/Author.java
@@ -0,0 +1,27 @@
+package ru.mirea.ikbo1319.pr4.z2;
+
+public class Author {
+    String name, email;
+    char gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
