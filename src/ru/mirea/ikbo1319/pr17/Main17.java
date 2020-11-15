8310d5bbf23a06a1c9cbffa606bc7dc021d44a0b
Александр Филиппов <xperiaz1981>
2020-11-15 20:58:11 +0300

    pr17 крестики нолики

--- src/ru/mirea/ikbo1319/pr17/Main17.java
+++ src/ru/mirea/ikbo1319/pr17/Main17.java
@@ -1,4 +1,20 @@
 package ru.mirea.ikbo1319.pr17;
 
-public class Main17 {
+import ru.mirea.ikbo1319.pr8.z2.Window;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main17 extends JComponent {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("TIC-TAC-TOE");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(500, 500);
+        window.setLayout(new BorderLayout());
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        Game game = new Game();
+        window.add(game);
+    }
 }
